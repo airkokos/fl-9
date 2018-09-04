b1152f033e4d6e684edbbf25a8261b604afb8a63
Kostiantyn Dubinin <32912165+airkokos@users.noreply.github.com>
2018-09-04 23:21:14 +0300

    Add homework №11

---
+++ FE_9_11_homework_dom/homework/src/app.js
@@ -0,0 +1,10 @@
+const rootNode = document.getElementById('root');
+
+
+let listItemCounter = 0;
+let listMaxItems = 10;
+
+const addButton = rootNode.querySelector(`.add-btn`);
+const inputNewItem = document.querySelector('.input-item');
+const toDoList = document.querySelector('.todo-list');
+
